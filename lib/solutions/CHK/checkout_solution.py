ab39afcb50f667acda4886b8d2bd66f37d48c77d
sbx_user1051 <sbx_user1051@169.254.182.85>
2020-02-01 20:59:46 +0000

    Sat Feb 01 20:59:46 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -69,6 +69,26 @@ def checkout(skus):
         'D': 15,
         'E': 40,
         'F': 10,
+        'G': 20,
+        'H': 10,
+        'I': 35,
+        'J': 60,
+        'K': 80,
+        'L': 90,
+        'M': 15,
+        'N': 40,
+        'O': 10,
+        'P': 50,
+        'Q': 30,
+        'R': 50,
+        'S': 30,
+        'T': 20,
+        'U': 40,
+        'V': 50,
+        'W': 20,
+        'X': 90,
+        'Y': 10,
+        'Z': 50,
     }
 
     discount_offers = {
@@ -102,8 +122,3 @@ def checkout(skus):
         price += current_price
 
     return price
-
-
-
-
-
