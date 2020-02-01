09a05b5f407807cc317d3a0bc60a176db67e6836
sbx_user1051 <sbx_user1051@169.254.182.85>
2020-02-01 18:57:36 +0000

    Sat Feb 01 18:57:36 UTC 2020

---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,8 @@
+from solutions.HLO import hello_solution
+
+
+class TestSum:
+
+    def test_hello(self):
+        assert hello_solution.hello('John') == 'Hello, John!'
+        assert hello_solution.hello('Marie') == 'Hello, Marie!'
