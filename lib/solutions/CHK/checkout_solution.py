9db7df0c87b1ae22a2c8d666a509fa713b50255d
sbx_user1051 <sbx_user1051@169.254.182.85>
2020-02-01 20:07:49 +0000

    Sat Feb 01 20:07:49 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,23 +5,39 @@ from collections import Counter
 
 
 @dataclass
-class SpecialOffer:
+class DiscountOffer:
     number: int
     price: int
 
 
-def discount_offer_price_calculator(frequencies, special_offers):
+class MultibuyOffer:
+    number: int
+    other_item: str
+
+
+def multibuy_offer_price_calculator(frequencies, multibuy_offers):
     price = 0
 
+    for sku, multibuy_offer in multibuy_offers.items():
+        multibuy_offer = multibuy_offers[sku]
+        n_offers = frequencies[sku] // multibuy_offer.number
+        frequencies[]
+        frequencies[multibuy_offer.other_item] = max(0, frequencies[multibuy_offer.other_item] - n_offers)
+
+    return price
+
+
+def discount_offer_price_calculator(frequencies, discount_offers):
+    price = 0
     frequencies_copy = Counter()
 
     for sku, number in frequencies.items():
-        if sku in special_offers:
-            special_offer = special_offers[sku]
-            n_offers = number // special_offer.number
-            leftovers = number % special_offer.number
+        if sku in discount_offers:
+            discount_offer = discount_offers[sku]
+            n_offers = number // discount_offer.number
+            leftovers = number % discount_offer.number
 
-            price += n_offers * special_offer.price
+            price += n_offers * discount_offer.price
             frequencies_copy[sku] = leftovers
 
         else:
@@ -52,9 +68,9 @@ def checkout(skus):
         'E': 40,
     }
 
-    special_offers = {
-        'A': SpecialOffer(number=3, price=130),
-        'B': SpecialOffer(number=2, price=45),
+    discount_offers = {
+        'A': DiscountOffer(number=3, price=130),
+        'B': DiscountOffer(number=2, price=45),
     }
 
     frequencies = Counter(skus)
@@ -66,10 +82,11 @@ def checkout(skus):
 
     price = 0
 
-    current_price, frequencies = discount_offer_price_calculator(frequencies, special_offers)
+    current_price, frequencies = discount_offer_price_calculator(frequencies, discount_offers)
     price += current_price
 
     current_price, frequencies = default_price_calculator(frequencies, prices)
     price += current_price
 
     return price
+
